4ab38da56330f7c8c5977bc2918d84df4174e291
Jaider Santiago Quimbay Avila <jsantiagoqavila@gmail.com>
2025-02-13 14:53:51 -0500

    Update ValidationStrategy.java

--- form-app/backend-java/src/main/java/com/app/utils/ValidationStrategy.java
+++ form-app/backend-java/src/main/java/com/app/utils/ValidationStrategy.java
@@ -1,8 +1,22 @@
+/**
+ * This file contains the interface for different question validation strategies.
+ * 
+ * Author: Daniel Santiago Pérez <dsperezm@udistrital.edu.co>
+ * Author: Jaider Santiago Avila Robles <jsquimbaya@udistrotal.edu.co>
+ */
 package com.app.utils;
 
 /**
- * Interface for different question validation strategies.
+ * Interface for different question validation strategies. Implementations of this
+ * interface define how to validate responses for specific types of questions.
  */
 public interface ValidationStrategy {
+
+    /**
+     * Validates a response based on the specific strategy.
+     *
+     * @param response The response to validate.
+     * @return True if the response is valid, false otherwise.
+     */
     boolean validate(String response);
 }
