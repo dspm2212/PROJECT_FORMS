009ace9c2979269deea8e251396dbb599ba797a4
Jaider Santiago Quimbay Avila <jsantiagoqavila@gmail.com>
2025-02-13 14:48:02 -0500

    Update FileManager.java

--- form-app/backend-java/src/main/java/com/app/services/FileManager.java
+++ form-app/backend-java/src/main/java/com/app/services/FileManager.java
@@ -1,16 +1,32 @@
+/**
+ * This file contains the definition of the class for file handling.
+ * 
+ * Author: Daniel Santiago Pérez <dsperezm@udistrital.edu.co>
+ * Author: Jaider Santiago Avila Robles <jsquimbaya@udistrotal.edu.co>
+ */
 package com.app.services;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Singleton class for file handling.
+ * Singleton class for handling file operations. This class provides methods
+ * for saving and loading data to/from files.
  */
 public class FileManager {
-    private static FileManager instance;
+    private static FileManager instance; // Singleton instance
 
+    /**
+     * Private constructor to prevent instantiation from outside the class.
+     */
     private FileManager() {}
 
+    /**
+     * Returns the singleton instance of FileManager. If the instance does not exist,
+     * it is created.
+     *
+     * @return The singleton instance of FileManager.
+     */
     public static FileManager getInstance() {
         if (instance == null) {
             instance = new FileManager();
@@ -18,6 +34,12 @@ public class FileManager {
         return instance;
     }
 
+    /**
+     * Saves the given content to a file.
+     *
+     * @param filename The name of the file to save the content to.
+     * @param content The content to save.
+     */
     public void saveToFile(String filename, String content) {
         try (FileWriter writer = new FileWriter(filename)) {
             writer.write(content);
