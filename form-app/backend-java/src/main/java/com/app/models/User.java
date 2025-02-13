22cdb4bc8a0244bc42f7c85efa6667c5dcedc37a
Jaider Santiago Quimbay Avila <jsantiagoqavila@gmail.com>
2025-02-13 14:43:54 -0500

    Update User.java

--- form-app/backend-java/src/main/java/com/app/models/User.java
+++ form-app/backend-java/src/main/java/com/app/models/User.java
@@ -1,26 +1,68 @@
-package com.app.models;
 /**
- * Represents a system user.
+ * This file contains the definition of the class for users.
+ * 
+ * Author: Daniel Santiago Pérez <dsperezm@udistrital.edu.co>
+ * Author: Jaider Santiago Avila Robles <jsquimbaya@udistrotal.edu.co>
+ */
+package com.app.models;
+
+/**
+ * Represents a system user. This class stores the user's ID, first name, last name, and email.
  */
 public class User {
-    private static int idCounter = 1;
-    private String id;
-    private String firstName;
-    private String lastName;
-    private String email;
+    private static int idCounter = 1; 
+    private String id; 
+    private String firstName; 
+    private String lastName; 
+    private String email; 
 
+    /**
+     * Constructs a new User with the specified first name, last name, and email.
+     *
+     * @param firstName The first name of the user.
+     * @param lastName The last name of the user.
+     * @param email The email of the user.
+     */
     public User(String firstName, String lastName, String email) {
-        this.id = String.valueOf(idCounter++);
+        this.id = String.valueOf(idCounter++); // Generate a unique ID for the user
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
+    /**
+     * Returns the ID of the user.
+     *
+     * @return The user ID.
+     */
     public String getId() { return id; }
+
+    /**
+     * Returns the first name of the user.
+     *
+     * @return The first name.
+     */
     public String getFirstName() { return firstName; }
+
+    /**
+     * Returns the last name of the user.
+     *
+     * @return The last name.
+     */
     public String getLastName() { return lastName; }
+
+    /**
+     * Returns the email of the user.
+     *
+     * @return The email.
+     */
     public String getEmail() { return email; }
 
+    /**
+     * Returns a string representation of the user.
+     *
+     * @return A string containing the user's details.
+     */
     @Override
     public String toString() {
         return "User{id='" + id + "', name='" + firstName + " " + lastName + "', email='" + email + "'}";
