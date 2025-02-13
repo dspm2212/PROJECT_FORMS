7cba577fe4f7b72eeb0e827d560f53b1d76dec38
Jaider Santiago Quimbay Avila <jsantiagoqavila@gmail.com>
2025-02-13 14:48:21 -0500

    Update FormService.java

--- form-app/backend-java/src/main/java/com/app/services/FormService.java
+++ form-app/backend-java/src/main/java/com/app/services/FormService.java
@@ -1,13 +1,26 @@
+/**
+ * This file contains the definition of the service class for forms.
+ * 
+ * Author: Daniel Santiago Pérez <dsperezm@udistrital.edu.co>
+ * Author: Jaider Santiago Avila Robles <jsquimbaya@udistrotal.edu.co>
+ */
 package com.app.services;
 
 import org.springframework.stereotype.Service;
 
 /**
- * Service class for handling form operations.
+ * Service class for handling form operations. This class provides methods
+ * for saving form data.
  */
 @Service
 public class FormService {
+
+    /**
+     * Saves the provided form data to a file.
+     *
+     * @param formData The form data to save.
+     */
     public void saveFormData(String formData) {
-        FileManager.getInstance().saveToFile("data\form_data.json", formData);
+        FileManager.getInstance().saveToFile("form_data.json", formData);
     }
 }
